bae5b326f947f938d0e9572801b8ac40ba74ca65
YanikMeier <hans.bernhard@byom.de>
2019-03-26 19:16:41 +0100

    uploads

--- server.js
+++ server.js
@@ -1,60 +1,50 @@
-/*const express = require('express');
-const app = express();
+const express = require('express');
+const multer = require('multer');
+const ejs = require('ejs');
 const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function(req, file, cb){
+        cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+});
+
+const upload = multer({
+    storage: storage
+}).single('myImage');
+
+
+// Init app
+const app = express();
+
+// EJS
+app.set('view engine', 'ejs');
+
+// Public Folder
+app.use(express.static('./public'));
+
+
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if(err){
+            res.render('index', {
+                msg: err
+            });
+        }else {
+            console.log(req.file);
+            res.send('test');
+        }
+    });    
+});
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
+    
+const port = 3000;
 
-app.listen(process.env.PORT || 4000, function(){
-    console.log('Your node js server is running');
-});*/
+app.listen(port, () => console.log(`server started on port ${port}`));
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var profile = require('./routes/profile');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-app.use('/profile', profile);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-
-module.exports = app;
